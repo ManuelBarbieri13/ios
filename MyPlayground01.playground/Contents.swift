fedfdb0e17c2ebd0f141da907aa2454a554b8ae8
ManuelBarbieri13 <49125260+ManuelBarbieri13@users.noreply.github.com>
2019-04-01 21:36:25 -0500

    Add files via upload

---
+++ MyPlayground01.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var str = "Hello, playground"
+let name = "Manuel"
+var myAge = 30
+myAge += 2
+
+// Ejercicio 01: Crear una constante de tipo Float, cuyo valor es 4 de manera explícita
+
+let explicitFloat : Float = 4
+
+let label = "Mi edad es " + String(myAge) // Se usa el constructor de String que recibe un int
+
+
+
+
+
+
